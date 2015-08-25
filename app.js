930d80e3285cceb4cbf4617c94c5e8bc8107c1f1
Grae Drake <graemdrake@gmail.com>
2015-08-25 14:22:41 -0400

    Refactor button event handler

--- app.js
+++ app.js
@@ -1,65 +1,69 @@
 var unitLibrary = [];
 $(document).ready(function() {
+
+    // Get the data from Highgrounds git repo and populate results.
     function getUnits() {
 
         var indexURL = "https://raw.githubusercontent.com/highgrounds/HighgroundsAssets/master/data/1stEdition.xml";
         var unitData = $.get(indexURL, function() {
-
-            // Get the card data as XML, parse it, and convert it to JSON.
-            var xmlString = unitData.responseText;
-            var parser = new DOMParser();
-            var xml = parser.parseFromString(xmlString, "text/xml");
-            cardJson = xmlToJson(xml);
-
-            // Build an object from the JSON data holding all cards.
-            // Omit the first placeholder unit.
-            rawCards = cardJson.data.CARDLIST.CARD;
-            console.dir(rawCards);
-            var cards = [];
-            for (var i = 1 ; i < rawCards.length ; i++) {
-                var rawCard = rawCards[i];
-                var name = rawCard.attributes.name;
-                var attributes = rawCard.attributes;
-                var actions = [];
-                var types = [];
-                var card = {
-                    "name": attributes.name,
-                    "cost": {"gold": attributes.g, "crystal": attributes.c, "wood": attributes.w},
-                    "edition": attributes.edition,
-                    "id": attributes.id,
-                    "rarity": attributes.rarity
-                };
-
-                // Each card can have several actions.
-                if (rawCard.hasOwnProperty("ACTION")) {
-                    for (var j = 0 ; j < rawCard.ACTION.length ; j ++) {
-                        actions.push(rawCard.ACTION[j].attributes);
-                    }
-                    card["actions"] = actions;
-                }
-
-                // Each card can have one or more types.
-                if (rawCard.hasOwnProperty("TYPE")) {
-                    if ($.isArray(rawCard.TYPE)) {
-                        for (var k = 0 ; k < rawCard.TYPE.length ; k++) {
-                            types.push(rawCard.TYPE[k]["#text"]);
-                        }
-                    } else if (typeof(rawCard.TYPE) === "object") {
-                        types.push(rawCard.TYPE["#text"]);
-                    }
-
-                    card["types"] = types;
-                }
-                cards.push(card);
-            }
-
-            // Push the result out to our unitLibrary global variable.
-            unitLibrary = cards;
-            populate(unitLibrary);
+            unitLibrary = parseHighgroundsXml(unitData);
+            populatePage(unitLibrary);
         });
     }
+    
+    function parseHighgroundsXml(unitData) {
 
-    function populate(units) {
+        // Get the card data as XML, parse it, and convert it to JSON.
+        var xmlString = unitData.responseText;
+        var parser = new DOMParser();
+        var xml = parser.parseFromString(xmlString, "text/xml");
+        cardJson = xmlToJson(xml);
+
+        // Build an array of objects from the JSON data holding all cards.
+        // Omit the first placeholder unit.
+        var cards = [];
+        rawCards = cardJson.data.CARDLIST.CARD;
+
+        for (var i = 1 ; i < rawCards.length ; i++) {
+            var rawCard = rawCards[i];
+            var name = rawCard.attributes.name;
+            var attributes = rawCard.attributes;
+            var actions = [];
+            var types = [];
+            var card = {
+                "name": attributes.name,
+                "cost": {"gold": attributes.g, "crystal": attributes.c, "wood": attributes.w},
+                "edition": attributes.edition,
+                "id": attributes.id,
+                "rarity": attributes.rarity
+            };
+
+            // Each card can one or more actions.
+            if (rawCard.hasOwnProperty("ACTION")) {
+                for (var j = 0 ; j < rawCard.ACTION.length ; j ++) {
+                    actions.push(rawCard.ACTION[j].attributes);
+                }
+                card["actions"] = actions;
+            }
+
+            // Each card can have one or more types.
+            if (rawCard.hasOwnProperty("TYPE")) {
+                if ($.isArray(rawCard.TYPE)) {
+                    for (var k = 0 ; k < rawCard.TYPE.length ; k++) {
+                        types.push(rawCard.TYPE[k]["#text"]);
+                    }
+                } else if (typeof(rawCard.TYPE) === "object") {
+                    types.push(rawCard.TYPE["#text"]);
+                }
+
+                card["types"] = types;
+            }
+            cards.push(card);
+        }
+        return cards;
+    }
+
+    function populatePage(units) {
         
         rarities = {
             0: "Common",
@@ -85,26 +89,31 @@ $(document).ready(function() {
         }
     }
 
-    // Event handlers.
-    $('button').on('click', function() {
-        $this = $(this);
-        $this.toggleClass('button-primary');
-
+    function getActiveResources() {
         activeButtons = $('.button-primary');
         activeResources = [];
         for (var i = 0 ; i < activeButtons.length ; i++) {
             activeResources.push($(activeButtons[i]).data("resourceType"));
         }
+        return activeResources;
+    }
 
-        var filteredUnits = unitLibrary.filter(function(unit) {
-            for (var j = 0 ; j < activeResources.length ; j ++) {
-                if (unit.cost[activeResources[j]] > 0) {
+    function filterUnits(unitLibrary, activeResources) {
+        return unitLibrary.filter(function(unit) {
+            for (var i = 0 ; i < activeResources.length ; i ++) {
+                if (unit.cost[activeResources[i]] > 0) {
                     return true;
                 }
             }
         });
+    }
+    // Event handlers.
+    $('button').on('click', function() {
+        $(this).toggleClass('button-primary');
+
+        activeResources = getActiveResources();
         $('.units').empty();
-        populate(filteredUnits);
+        populatePage(filterUnits(unitLibrary, activeResources));
 
     });
     getUnits();
