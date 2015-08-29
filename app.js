6bf7cd835cbb30903776dfcad275882c856740ce
Raijinili <Raijinili@users.noreply.github.com>
2015-08-29 00:35:31 -0400

    Pulled some code into functions; card memoization
    Also made sure that a card's homeActions, battleActions, and types were always arrays instead of sometimes undefined.

--- app.js
+++ app.js
@@ -22,69 +22,18 @@ $(document).ready(function() {
         // Build an array of objects from the JSON data holding all cards.
         // Omit the first placeholder unit.
         var cards = [];
-        rawCards = cardJson.data.CARDLIST.CARD;
-        rarities = {
+        var rawCards = cardJson.data.CARDLIST.CARD;
+        var rarities = {
             0: "Common",
             1: "Uncommon",
             2: "Rare",
             3: "Ultra Rare",
             4: "Legendary"
         };
-
-        for (var i = 1 ; i < rawCards.length ; i++) {
-            var rawCard = rawCards[i];
-            var name = rawCard.attributes.name;
-            var attributes = rawCard.attributes;
-            var homeActions = [];
-            var battleActions = [];
-            var types = [];
-            var card = {
-                "name": attributes.name,
-                "cost": {"gold": parseInt(attributes.g, 10),
-                         "crystal": parseInt(attributes.c, 10),
-                        "wood": parseInt(attributes.w, 10)},
-                "edition": attributes.edition,
-                "id": attributes.id,
-                "rarity": rarities[parseInt(attributes.rarity, 10)]
-            };
-
-            // Each card can one or more home or battle actions.
-            if (rawCard.hasOwnProperty("ACTION")) {
-                for (var j = 0 ; j < rawCard.ACTION.length ; j ++) {
-                    var action = rawCard.ACTION[j].attributes;
-                    var target = action.location === "home" ? homeActions : battleActions;
-                    
-                    // Deal with weird action values like -9999 for "X" and
-                    // -1000 for actions like frail and dormant.
-                    var actionValue = action.value === "-9999" ? "X" :
-                                      action.value === "-1000" ? "" :
-                                      action.value;
-
-                    // Deal with weird action types like Windfall
-                    var actionType = action.type.indexOf("windfall") > -1 ? "windfall" :
-                                     action.type.indexOf("duorainer") > -1 ? "duorainer" :
-                                     action.type.indexOf("duoRainer") > -1 ? "duorainer" :
-                                     action.type;
-
-                    target.push({"type": actionType, "value": actionValue});
-                }
-                card["homeActions"] = homeActions;
-                card["battleActions"] = battleActions;
-
-            }
-
-            // Each card can have one or more types.
-            if (rawCard.hasOwnProperty("TYPE")) {
-                if ($.isArray(rawCard.TYPE)) {
-                    for (var k = 0 ; k < rawCard.TYPE.length ; k++) {
-                        types.push(rawCard.TYPE[k]["#text"]);
-                    }
-                } else if (typeof(rawCard.TYPE) === "object") {
-                    types.push(rawCard.TYPE["#text"]);
-                }
-
-                card["types"] = types;
-            }
+        
+        // Start from i=1 to skip the empty card.
+        for (var i = 1, len = rawcards.length ; i < len ; i++) {
+            var card = parseCard(rawcards[i]);
             cards.push(card);
         }
         return cards;
@@ -101,10 +50,8 @@ $(document).ready(function() {
 
     function filterUnits(unitLibrary, activeResources) {
         return unitLibrary.filter(function(unit) {
-            for (var i = 0 ; i < activeResources.length ; i ++) {
-                if (unit.cost[activeResources[i]] > 0) {
-                    return true;
-                }
+            activeResources.some(function(resource){
+                return unit.cost[resource] > 0;
             }
         });
     }
@@ -128,11 +75,79 @@ $(document).ready(function() {
         var theTemplateScript = $("#unit-card").html();
         var theTemplate = Handlebars.compile(theTemplateScript);
 
-        for (var i = 0 ; i < units.length ; i++) {
+        for (var i = 0, len = units.length; i < len; i++) {
             var unit = units[i];
             console.log(unit.name, unit);
-        $(".units").append(theTemplate(unit));
+            
+            // easy memoization of the template result
+            var unitDisplay = unit["_cache"];
+            if (unitDisplay === undefined) {
+                unitDisplay = unit["_cache"] = theTemplate(unit);
+            }
+            
+            $(".units").append(unitDisplay);
         }
 
     }
+    
+    function parseCard(rawCard){
+        
+        var name = rawCard.attributes.name;
+        var attributes = rawCard.attributes;
+        var homeActions = [];
+        var battleActions = [];
+        var types = [];
+        var card = {
+            "name": attributes.name,
+            "cost": {"gold": parseInt(attributes.g, 10),
+                     "crystal": parseInt(attributes.c, 10),
+                     "wood": parseInt(attributes.w, 10)},
+            "edition": attributes.edition,
+            "id": attributes.id,
+            "rarity": rarities[parseInt(attributes.rarity, 10)],
+            "homeActions": homeActions,
+            "battleActions": battleActions,
+            "types": types,
+        };
+
+        // Each card can one or more home or battle actions.
+        if (rawCard.hasOwnProperty("ACTION")) {
+            for (var j = 0; j < rawCard.ACTION.length; j++) {
+                var action = rawCard.ACTION[j].attributes;
+                var target = action.location === "home" ? homeActions : battleActions;
+                
+                target.push(parseAction(action));
+            }
+        }
+
+        // Each card can have one or more types.
+        if (rawCard.hasOwnProperty("TYPE")) {
+            var rawTypes = rawCard.TYPE;
+            if ($.isArray(rawTypes)) {
+                for (var k = 0 ; k < rawTypes.length ; k++) {
+                    types.push(rawTypes[k]["#text"]);
+                }
+            } else if (typeof(rawTypes) === "object") {
+                types.push(rawTypes["#text"]);
+            }
+        }
+    }
+
+    // Returns a dict for the action: {type:, value:}
+    function parseAction(action){
+        // Deal with weird action values like -9999 for "X" and
+        // -1000 for actions like frail and dormant.
+        var actionValue = action.value === "-9999" ? "X" :
+                          action.value === "-1000" ? "" :
+                          action.value;
+
+        // Deal with weird action types like Windfall
+        var actionType = action.type.indexOf("windfall") > -1 ? "windfall" :
+                         action.type.indexOf("duorainer") > -1 ? "duorainer" :
+                         action.type.indexOf("duoRainer") > -1 ? "duorainer" :
+                         action.type;
+        
+        return {"type": actionType, "value": actionValue};
+    }
+
 });
